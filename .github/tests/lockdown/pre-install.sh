ca1bea5a4f8acfe9e6dd52abb652abb004c40664
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-03-03 04:40:44 -0800

    Reorder test
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/lockdown/pre-install.sh
+++ .github/tests/lockdown/pre-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl label namespace "$VALUES" pod-security.kubernetes.io/enforce=restricted
-kubectl create namespace "${VALUES}-system"
-kubectl label namespace "${VALUES}-system" pod-security.kubernetes.io/enforce=privileged
+kubectl label namespace "$VALUES" pod-security.kubernetes.io/enforce=privileged
+kubectl create namespace "${VALUES}-deps"
+kubectl label namespace "${VALUES}-deps" pod-security.kubernetes.io/enforce=restricted
